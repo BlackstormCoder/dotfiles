5184cedf6268c76026c17870390e855825840606
Your Name <you@example.com>
2024-12-24 12:48:25 +0530

    updated

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -73,9 +73,7 @@ local plugins = {
   {
     'tpope/vim-commentary'
   },
-  { "nvim-neotest/nvim-nio" },
   {
-
     "kylechui/nvim-surround",
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
